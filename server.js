7f569b31560735c79c0ba0f1d19544330da356df
Abhijeet kumar <krabhijeet0225@gmail.com>
2026-05-15 17:19:41 +0530

    fix vercel server

--- server.js
+++ server.js
@@ -70,7 +70,5 @@ app.post("/generate", async (req, res) => {
 });
 
 // ---------------- START SERVER ----------------
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("🚀 Server running on port " + PORT);
-});
+module.exports = app;
+
